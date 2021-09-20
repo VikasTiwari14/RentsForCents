0ec3c53fa5e58701421683b1391dbbdd239fda7a
Harshit Raj <harshitraj4839@gmail.com>
2021-09-20 13:05:25 +0530

    Bike Details Added, Prepared for Manager Dashboard

---
+++ backend/src/database/bikeDB.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+mongoose.connect("mongodb+srv://harshitraj12:harshit@cluster0.d755c.mongodb.net/mainDB?retryWrites=true&w=majority").then(()=>console.log("Connection Successful")).catch((err)=>console.log(err))
+
+
+
+const bike_schema = new mongoose.Schema({
+    brandName:String,
+    modelNumber:String,
+    vehicleNumber:String,
+    rate:Number,
+    vehicleImage:String
+})
+
+const bike = new mongoose.model("bikeDetails",bike_schema)
+
+module.exports=bike
